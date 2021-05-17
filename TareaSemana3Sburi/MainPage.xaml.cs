8e90d0d9ea26b735cf10a08f54e662ccac7ae352
Sebassbs1991 <84051570+Sebassbs1991@users.noreply.github.com>
2021-05-17 00:01:48 -0500

    Agregar archivos de proyecto.

---
+++ TareaSemana3Sburi/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TareaSemana3Sburi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUser.Text))
+            {
+                await DisplayAlert("Error", "Ingrese usuario", "Aceptar");
+                txtUser.Focus();
+                return;
+
+            }
+
+            if (string.IsNullOrEmpty(txtPass.Text))
+            {
+                await DisplayAlert("Error", "Ingrese contraseña", "Aceptar");
+                txtPass.Focus();
+                return;
+            }
+
+            if (txtUser.Text == "estudiante2021" && txtPass.Text == "uisrael2021")
+            {
+                string usuario = txtUser.Text;
+                string clave = txtPass.Text;
+
+
+                await Navigation.PushAsync(new Notas(usuario, clave));
+
+
+            }
+            else
+            {
+                await DisplayAlert("Error", "Usuario o clave incorrecta", "Aceptar");
+            }
+
+        }
+    }
+}
---
+++ TareaSemana3Sburi/Notas.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TareaSemana3Sburi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Notas : ContentPage
+    {
+        double dato1;
+        double dato2;
+        double dato3;
+        double dato4;
+        double calcular;
+        double calcular2;
+        double notaFinal;
+
+        public Notas(string usuario, string clave)
+        {
+            InitializeComponent();
+
+            lblUser.Text = $"Usuario Conectado: {usuario}";
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                dato1 = Convert.ToDouble(txtDato1.Text);
+                dato2 = Convert.ToDouble(txtDato2.Text);
+
+                calcular = (dato1 * 0.3) + (dato2 * 0.2);
+                txtResultado.Text = calcular.ToString();
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Gracias");
+            }
+        }
+
+        private void btnCalcular2_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                dato3 = Convert.ToDouble(txtDato3.Text);
+                dato4 = Convert.ToDouble(txtDato4.Text);
+
+                calcular2 = (dato3 * 0.3) + (dato4 * 0.2);
+                txtResultado2.Text = calcular2.ToString();
+
+
+                notaFinal = calcular + calcular2;
+                txtNotaFinal.Text = notaFinal.ToString();
+
+                if (notaFinal >= 7)
+                {
+                    DisplayAlert("Alerta", "APROBADO", "Gracias");
+                }
+                else if (notaFinal < 7 && notaFinal > 5)
+                {
+                    DisplayAlert("Alerta", "COMPLEMENTARIO", "Gracias");
+                }
+                else
+                {
+                    DisplayAlert("Alerta", "REPROBADO", "Gracias");
+                }
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Gracias");
+            }
+
+        }
+    }
+}
